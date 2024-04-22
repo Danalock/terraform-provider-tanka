a15366d2a8cd99ce679312757263ce1e4b66412a
Nicolaj Aagaard Knudsen <nk@danalock.dk>
2024-04-22 14:36:20 +0200

    docs wip

---
+++ examples/main.tf
@@ -0,0 +1,47 @@
+variable "endpoint" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+
+variable "token" {
+  type = string
+}
+
+terraform {
+  required_providers {
+    tanka = {
+      source = "Danalock/tanka"
+    }
+  }
+}
+
+provider "tanka" {
+  endpoint               = var.endpoint
+  cluster_ca_certificate = var.cluster_ca_certificate
+  token                  = var.token
+}
+
+resource "tanka_release" "example" {
+  version = "456"
+  config = jsonencode({
+    key_1 : "value_1"
+    key_2 : "value_2"
+    key_list : [
+      "a", "few", "list", "items"
+    ],
+    key_nested : {
+      new : {
+        deep : "leaf"
+      }
+      string_value : "value"
+    },
+  })
+  # config_override = "file://tanka_config_override.json"
+  config_override = jsonencode({
+    key_1        = "overridden_value_1",
+    key_override = "value_only_existing_in_override",
+  })
+}
--- main.go
+++ main.go
@@ -14,21 +14,18 @@ import (
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
 
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
-// ensure the documentation is formatted properly.
+// If you do not have terraform installed, you can remove the formatting command, but its suggested to ensure the documentation is formatted properly.
 //go:generate terraform fmt -recursive ./examples/
 
-// Run the docs generation tool, check its repository for more information on how it works and how docs
-// can be customized.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name scaffolding
+// Run the docs generation tool, check its repository for more information on how it works and how docs can be customized.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name tanka
 
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary.
 	version string = "dev"
 
-	// goreleaser can pass other information to the main package, such as the specific commit
-	// https://goreleaser.com/cookbooks/using-main.version/
+	// goreleaser can pass other information to the main package, such as the specific commit https://goreleaser.com/cookbooks/using-main.version/
 )
 
 func main() {
@@ -38,8 +35,6 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// Also update the tfplugindocs generate command to either remove the
-		// -provider-name flag or set its value to the updated provider name.
 		Address: "registry.terraform.io/Danalock/tanka",
 		Debug:   debug,
 	}
